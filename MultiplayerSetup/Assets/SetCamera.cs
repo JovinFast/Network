10a583c36547bc261aee7d9da5dfa73e00eafed5
LucasKarjel <lucas.karjel@gmail.com>
2023-05-16 15:47:42 +0200

    avatarcheck

--- MultiplayerSetup/Assets/SetCamera.cs
+++ MultiplayerSetup/Assets/SetCamera.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Alteruna;
 
 public class SetCamera : MonoBehaviour
 {
     CameraFollow cameraFollowScript;
     [SerializeField] GameObject camFollow;
+    Alteruna.Avatar avatar;
     private void Awake()
     {
         cameraFollowScript = FindAnyObjectByType<CameraFollow>();
+        avatar = GetComponent<Alteruna.Avatar>();
 
     }
     private void Start()
     {
+        if (!avatar.IsMe)
+        {
+            enabled = false;
+            return;
+        }
         CollectObjectsWithTag();
     }
     private void CollectObjectsWithTag()
@@ -27,6 +35,4 @@ public class SetCamera : MonoBehaviour
             }
         }
     }
-
-
 }
