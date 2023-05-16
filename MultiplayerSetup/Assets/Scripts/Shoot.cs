9311b27392f21106285b4839a35b83e38bb03ddc
JovinFast <112895166+JovinFast@users.noreply.github.com>
2023-05-16 10:50:18 +0200

    ReloadSpeed

--- MultiplayerSetup/Assets/Scripts/Shoot.cs
+++ MultiplayerSetup/Assets/Scripts/Shoot.cs
@@ -10,11 +10,19 @@ public class Shoot : MonoBehaviour
     public Alteruna.Avatar avatar;
     private Spawner spawner;
 
+    [SerializeField] float decreaseAmount;
+    
+    private float reloadSpeed = 2;
+    private float reloadSpeedStart;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         spawner = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<Spawner>();
         avatar= GetComponent<Alteruna.Avatar>();
+        reloadSpeedStart = reloadSpeed;
+        reloadSpeed = 0;
     }
 
     // Update is called once per frame
@@ -22,10 +30,12 @@ public class Shoot : MonoBehaviour
     {
         if (!avatar.IsMe)
             return;
-
+        reloadSpeed -=  decreaseAmount * Time.deltaTime;
+        if (reloadSpeed > 0) return; 
         if(Input.GetMouseButtonDown(0)) 
         {
             spawner.Spawn(1, pivotPoint.transform.position, pivotPoint.transform.rotation);
+            reloadSpeed = reloadSpeedStart;
             //Instantiate(bulletPrefab, pivotPoint.transform.position, pivotPoint.transform.rotation);
         }
     }
