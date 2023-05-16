eb5d426ec39427db06498fe5fc9fbe78b863fc05
JovinFast <112895166+JovinFast@users.noreply.github.com>
2023-05-16 12:37:18 +0200

    Health and reload pickups

---
+++ MultiplayerSetup/Assets/Scripts/PowerUp/HealthPickUp.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Alteruna;
+
+public class HealthPickUp : AttributesSync
+{
+    int healthAmount = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other.TryGetComponent(out Alteruna.Avatar _avatar) && _avatar.IsMe)
+            {
+                if (other.GetComponent<Alteruna.Avatar>().IsMe)
+                {
+                    if(other.GetComponent<Health>().currentHealth < 10)
+                    {
+                        other.GetComponent<Health>().currentHealth += healthAmount;
+                    }
+                    else if (other.GetComponent<Health>().currentHealth > 10)
+                    {
+                        other.GetComponent<Health>().currentHealth = 10;
+                    }
+                }
+            }
+            InvokeRemoteMethod(nameof(DestroyPowerUp), UserId.AllInclusive);
+        }
+    }
+    [SynchronizableMethod]
+    private void DestroyPowerUp()
+    {
+        Destroy(gameObject);
+    }
+
+}
