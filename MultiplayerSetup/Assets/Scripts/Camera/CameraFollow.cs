ebc6cab2283840e43ec528f99c9868904472811c
LucasKarjel <lucas.karjel@gmail.com>
2023-05-16 14:46:05 +0200

    Camstuff

---
+++ MultiplayerSetup/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform camTarget;
+
+    private float pLerp = 0.02f;
+    private float rLerp = 0.01f;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (camTarget == null)
+        {
+            camTarget = GameObject.Find("camFollow").transform;
+            if (camTarget.parent.GetComponent<Alteruna.Avatar>().IsMe)
+            {
+
+            }
+            camTarget = GameObject.Find("camFollow").transform;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, camTarget.position, pLerp);
+        transform.rotation = Quaternion.Lerp(transform.rotation, camTarget.rotation, rLerp);
+    }
+
+    void ApplyCamera(GameObject player)
+    {
+        camTarget = player.transform;
+    }
+}
