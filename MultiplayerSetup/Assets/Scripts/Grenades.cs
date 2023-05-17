a1e3ab4eeb69e752d3bf7ea18624a7eed2f6bb02
LucasKarjel <lucas.karjel@gmail.com>
2023-05-17 11:36:57 +0200

    grenades

--- MultiplayerSetup/Assets/Scripts/Grenades.cs
+++ MultiplayerSetup/Assets/Scripts/Grenades.cs
@@ -10,49 +10,38 @@ public class Grenades : MonoBehaviour
     public Alteruna.Avatar avatar;
     private Spawner spawner;
 
-    [SerializeField] float decreaseAmount;
-
     public float chargeUp;
     public float maxCharge;
-    private float reloadSpeed = 2;
-    public float reloadSpeedStart;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        if (!avatar.IsMe)
+        {
+            enabled = false;
+            return;
+        }
         spawner = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<Spawner>();
         avatar = GetComponent<Alteruna.Avatar>();
-        reloadSpeedStart = reloadSpeed;
-        reloadSpeed = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!avatar.IsMe)
-            return;
-        reloadSpeed -= decreaseAmount * Time.deltaTime;
-        if (reloadSpeed > 0) return;
-        if (Input.GetMouseButtonDown(1))
+        //if (!avatar.IsMe)
+        //    return;
+        if (Input.GetMouseButton(1))
         {
-            spawner.Spawn(1, pivotPoint.transform.position, pivotPoint.transform.rotation);
-            reloadSpeed = reloadSpeedStart;
+            while (chargeUp < maxCharge)
+            {
+                chargeUp += Time.deltaTime;
+            }
+            if (Input.GetMouseButtonUp(1) && chargeUp > 0)
+            {
+                spawner.Spawn(1, pivotPoint.transform.position, pivotPoint.transform.rotation);
+            }
             //Instantiate(bulletPrefab, pivotPoint.transform.position, pivotPoint.transform.rotation);
         }
-    }
-
-    public void DecreaseReloadSpeed()
-    {
-        if (reloadSpeedStart > 0)
-        {
-            reloadSpeedStart -= 0.3f;
-        }
-    }
-
-    public void ResetFireRate()
-    {
-        reloadSpeedStart = 2;
-        reloadSpeed = 2;
     }
 }
